cedec622474e4512d137afcd6614c6208194b3ae
Elizabeth Earl <elizabethearl@elizabeths-macbook-air.local>
2016-09-24 11:42:33 -0600

    Initial Commit

---
+++ StyleBy...Tests/StyleBy___Tests.swift
@@ -0,0 +1,36 @@
+//
+//  StyleBy___Tests.swift
+//  StyleBy...Tests
+//
+//  Created by Elizabeth Earl on 9/24/16.
+//  Copyright © 2016 Jason. All rights reserved.
+//
+
+import XCTest
+@testable import StyleBy___
+
+class StyleBy___Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
